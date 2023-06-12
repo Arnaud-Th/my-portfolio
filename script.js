0363229589fe50807cfd334fd669a36ec5e94565
Arnaud Theard <arnaudtheard64@hotmail.fr>
2023-06-12 19:25:34 +0700

    Cards functionalities added

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+/* Arrow functions */
+
 const arrowTop = document.querySelector(".scrolltop");
 
 const handleScrollPage = (event) => {
@@ -14,3 +17,61 @@ const handleScrollTop = () => window.scrollTo({top:0, behavior: "smooth"});
 
 window.addEventListener('scroll', handleScrollPage)
 arrowTop.addEventListener('click', handleScrollTop)
+
+
+/* Cards displaying functions */
+
+const cardOne = document.getElementById("card-one");
+
+function displayOne(event) {
+    document.getElementById("whole-card-one").style.display = "block";
+}
+
+cardOne.addEventListener('click', displayOne)
+
+
+const cardTwo = document.getElementById("card-two");
+
+function displayTwo(event) {
+    document.getElementById("whole-card-two").style.display = "block";
+}
+
+cardTwo.addEventListener('click', displayTwo)
+
+
+const cardThree = document.getElementById("card-three");
+
+function displayThree(event) {
+    document.getElementById("whole-card-three").style.display = "block";
+}
+
+cardThree.addEventListener('click', displayThree)
+
+
+/* Cards hiding functions */
+
+const wholeCardOne = document.getElementById("whole-card-one");
+
+function hideOne(event) {
+    document.getElementById("whole-card-one").style.display = "none";
+}
+
+wholeCardOne.addEventListener('click', hideOne)
+
+
+const wholeCardTwo = document.getElementById("whole-card-two");
+
+function hideTwo(event) {
+    document.getElementById("whole-card-two").style.display = "none";
+}
+
+wholeCardTwo.addEventListener('click', hideTwo)
+
+
+const wholeCardThree = document.getElementById("whole-card-three");
+
+function hideThree(event) {
+    document.getElementById("whole-card-three").style.display = "none";
+}
+
+wholeCardThree.addEventListener('click', hideThree)
